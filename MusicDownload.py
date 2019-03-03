bf961b77574e22251f51dd76d2141ef2be797173
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2019-03-03 17:54:34 -0300

    Update MusicDownload.py

--- MusicDownload.py
+++ MusicDownload.py
@@ -1,45 +1,37 @@
 # -*- coding: utf-8 -*-
-#Devidos créditos Miqueias
-#https://www.facebook.com/miqueias.costa.754
+#!/usr/bin/python
 from pytube  import YouTube
 from pytube  import Playlist
-
-print ("deseja baixar uma playlist ou apenas musicas separadas")
-print("Digite 1 para muscas separadas e 2 para playlist")
-
-pl = int(input('Digite: '))
-
-if (pl == 1):
-     while True:
-          print("Você escolheu musicas separadas.")
-          yt = str(input('cole a URL do video: '))
-          ur = YouTube(yt)
-          tt = ur.title
-          ck =ur.thumbnail_url
-          tk = ur.streams.get_by_itag(140)
-          print("Baixando...")
-          print(tt)
-          tk.download()
-
-if (pl == 2):
-     print("Você escolheu Playlis.")
-     pt = str(input("Link da palylist para ser baixada: "))
-     ply = Playlist(pt)
-     print("Aguarde enquanto a plylist esta sendo baixada...")
-     print("OBS: Tudo depende da sua internet.")
-     ply.download_all()
-     
-
-else:
-     print("By Miqueias <3")
-     exit()
-    
-
-     
-     
-
-
-     
+from pydub import AudioSegment
+import glob, os, time 
+from  tqdm  import *
 
 
 
+
+a = open("lista.txt","r")
+try:
+  for i in a.readlines():
+    yt = i
+    ur = YouTube(yt)
+    tt = ur.title
+    ck =ur.thumbnail_url
+    tk = ur.streams.get_by_itag(140)
+    print("Baixando...")
+    print(tt)
+    tk.download()
+except Exception:
+   print("\n")
+
+
+
+for i in glob.glob('*.mp4'):
+    ta = os.path.getsize(i)
+    f = (ta * 8)
+    sound = AudioSegment.from_file(i)
+    a = i.replace(".mp4", ".mp3")
+    print(sound.export(a, format="mp3"))
+
+os.system("rm -rf *.mp4 && clear")
+
+exit()
